e5540e0b67966b794c6552db1090aa3addfcf2c1
wenchao.hao <awenhaowenchao@hotmail.com>
2021-04-26 16:55:01 +0800

    Update setup.py
    v0.1.2
    modify async to async_func

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/py-bee/bee-util'
 EMAIL = 'awenhaowenchao@hotmail.com'
 AUTHOR = 'wenchao.hao'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 
 # What packages are requirpiped for this module to be executed?
 REQUIRED = [
